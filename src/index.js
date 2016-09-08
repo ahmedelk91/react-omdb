44a9e26ac8b742d71eb4fb84972ac280db4633a2
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-09-08 10:18:42 -0400

    changed filename of app.js to home.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Developer TODO: Load in App component and render to the DOM
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Hello from './App.js'
+import Hello from './Home.js'
 
 ReactDOM.render(
   <Hello />,
