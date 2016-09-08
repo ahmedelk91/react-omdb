519016c1b4d0f73e6a56612cd8fa5b26b15eb192
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-09-08 10:15:44 -0400

    added hello world component

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
 // Developer TODO: Define App component defintion here
+import React, {Component} from 'react'
+
+class Hello extends Component {
+  render() {
+    return(
+      <h1> Hello World! </h1>
+    )
+  }
+}
+
+export default Hello
